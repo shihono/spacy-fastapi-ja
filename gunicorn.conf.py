700a7b80d97e3b3ff2c1e2fb031055848db5f0a2
shihono <26952997+shihono@users.noreply.github.com>
2024-05-26 17:46:25 +0900

    add gunicorn

---
+++ gunicorn.conf.py
@@ -0,0 +1,6 @@
+# settings: https://docs.gunicorn.org/en/stable/settings.html
+max_requests = 1000
+max_requests_jitter = 100
+bind = "0.0.0.0:8080"
+workers = 2
+worker_class= "uvicorn.workers.UvicornWorker"
